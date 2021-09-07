6aefa3acb4f4ba433f3bff9ff8e9790136a9a186
Mikalai Konan <mikalai.konan@icloud.com>
2021-09-07 22:12:21 +0300

    socks5

--- src/server/index.js
+++ src/server/index.js
@@ -2,26 +2,46 @@ import express from "express";
 import cors from "cors";
 import compression from "compression";
 import { Router } from "express";
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createProxyMiddleware } from "http-proxy-middleware";
+import SocksProxyAgent from "socks-proxy-agent";
+
+const socksAddr = process.env.SOCKS_ADDR;
 
 const getServer = async (client) => {
-    const app = express();
-    const router = new Router();
+  const app = express();
+  const router = new Router();
 
-    app.use(cors());
-    app.use(compression());
-    app.use(express.json());
+  app.use(cors());
+  app.use(compression());
+  app.use(express.json());
 
-    router.get('/api/v3/klines', async (req, res) => {
-        client.getCandles(req.query.symbol, req.query.interval).then(result => res.send(result))
-    });
+  router.get("/api/v3/klines", async (req, res) => {
+    client
+      .getCandles(req.query.symbol, req.query.interval)
+      .then((result) => res.send(result));
+  });
 
-    router.use(createProxyMiddleware({
-        target: 'https://api.binance.com', changeOrigin: true
-    }));
+  let proxy;
 
-    app.use("/", router);
-    return app
-}
+  if (!!socksAddr) {
+    router.use(
+      createProxyMiddleware({
+        target: "https://ifconfig.me",
+        changeOrigin: true,
+        agent: new SocksProxyAgent(socksAddr),
+      })
+    );
+  } else {
+    router.use(
+      createProxyMiddleware({
+        target: "https://api.binance.com",
+        changeOrigin: true,
+      })
+    );
+  }
 
-export default getServer
+  app.use("/", router);
+  return app;
+};
+
+export default getServer;
